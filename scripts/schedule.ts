27d285598d2ebcf251b3421c83a390780548714b
Yu Peng Ng <ngyupeng06@gmail.com>
2023-01-14 18:53:32 +0800

    filterModule fixed

--- scripts/schedule.ts
+++ scripts/schedule.ts
@@ -1,3 +1,27 @@
-export const schedule = () =>{
+import { ModuleLesson } from "./utils";
+
+const taken: boolean[] = []
+const blocked: boolean[] = []
+
+type ClassSlot = {
+    moduleCode: string
+    lessonType: string
+    classNo: string
+}
+
+var classes: ModuleLesson[] = []
+
+classes.sort((a, b) => a.timings.size - b.timings.size)
+
+const possibleSchedules: ClassSlot[][] = []
+
+export const schedule = (idx: number) => {
+    const moduleCode: string = classes[idx].moduleCode
+    const tempStr: string = classes[idx].lessonType
+    let lessonType: string = ""
+    for (let i = 0; i < 3; i++) {
+        lessonType += tempStr.charAt(i)
+    }
+    const timings: Map<string, number[]> = classes[idx].timings
     
 }
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -18,23 +18,86 @@ type TimeData = {
     lessonType: string
 }
 
+type ModuleClasses = {
+    moduleCode: string
+    classes: Map<string, Map<string, number[]>>
+}
+
+export type ModuleLesson = {
+    moduleCode: string
+    lessonType: string
+    timings: Map<string, number[]>
+}
+
 export const fetchModule = async (moduleCode: string, semester: number): Promise<TimeData[]> => {
     const result: ModuleData = (await axios.get(`https://api.nusmods.com/v2/2022-2023/modules/${moduleCode}.json`)).data
     return result.semesterData[semester - 1].timetable
 }
 
-export const filterModule = async (moduleCode: string, semester: number): Promise<TimeData[]> => {
+export const filterModule = async (moduleCode: string, semester: number): Promise<ModuleLesson[]> => {
     const resData: TimeData[] = await fetchModule(moduleCode, semester)
-    const objData = new Map<string, boolean>()
     const timeArr: TimeData[] = []
-    for (const obj of resData) {
-        const { startTime, endTime, weeks, day, lessonType } = obj
-        const str = JSON.stringify({ startTime, endTime, weeks, day, lessonType })
-        if (objData.has(str)) {
-            continue
-        }
-        objData.set(str, true)
-        timeArr.push(obj)
+
+    const days: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+    const moduleClasses: ModuleClasses = {
+        moduleCode: moduleCode,
+        classes: new Map<string, Map<string, number[]>>()
     }
-    return timeArr
+
+    for (const obj of resData) {
+        const { classNo, startTime, endTime, weeks, day, lessonType } = obj
+        const startT: number = Number.parseInt(startTime) / 100 - 8
+        const endT: number = Number.parseInt(endTime) / 100 - 8
+        let dayNum: number = -1
+        
+        for (let i = 0; i < 5; i++) {
+            if (day === days[i]) {
+                dayNum = i
+            }
+        }
+
+        for (const i of weeks) {
+            for (let j = startT; j < endT; j++) {
+                const norm: number = j + dayNum * 5 + i * 35;
+                if (!moduleClasses.classes.has(lessonType)) {
+                    moduleClasses.classes.set(lessonType, new Map<string, number[]>())
+                }
+                if (!moduleClasses.classes.get(lessonType)?.has(classNo)) {
+                    moduleClasses.classes.get(lessonType)?.set(classNo, [])
+                }
+                moduleClasses.classes.get(lessonType)?.get(classNo)?.push(norm)
+            }
+        }
+    }
+
+    for (const [_, timings] of moduleClasses.classes) {
+        const classTimes: Map<string, boolean> = new Map<string, boolean>()
+        const toErase: string[] = []
+        for (const [classNo, timeArr] of timings) {
+            timeArr.sort()
+            const str = JSON.stringify(timeArr)
+            if (classTimes.has(str)) {
+                toErase.push(classNo)
+                continue
+            }
+            classTimes.set(str, true)
+        }
+        for (const classNo of toErase) {
+            timings.delete(classNo)
+        }
+    }
+
+    const moduleLessons: ModuleLesson[] = []
+    
+    for (const [lessonType, timings] of moduleClasses.classes) {
+        const moduleLesson: ModuleLesson = {
+            moduleCode: moduleCode,
+            lessonType: lessonType,
+            timings: timings
+        }
+        moduleLessons.push(moduleLesson)
+    }
+
+    return moduleLessons
 }
